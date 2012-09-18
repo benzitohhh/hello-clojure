16010449c466e6cc472d76b4779c906f44439b20
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-09-18 20:13:43 -0300

    factorial

--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -5,6 +5,15 @@
   [x]
   (println x "Hello, World!"))
 
+(defn factorial
+  "calculate the factorial of a number"
+  [x]
+  ((if (<= x 1)
+     1
+     (* x (factorial (- x 1))))))
+
 (require 'hello-clojure.core)
 
-(hello-clojure.core/foo 3)
+(hello-clojure.core/foo 777)
+
+(factorial 3)
